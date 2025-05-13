bf4b664b0a00211204e6cbf4445acd671dc158ea
Denis Šneider <denis.sneider@student.um.si>
2025-05-13 16:21:56 +0200

    Fix for tests 2

--- paymentService/jest.config.ts
+++ paymentService/jest.config.ts
@@ -1,5 +1,13 @@
-export default {
-    rootDir: '.',
-    testMatch: ['**/test/**/*.spec.ts'],
-  };
-  
+import type { Config } from 'jest';
+
+const config: Config = {
+  rootDir: '.',
+  testMatch: ['**/test/**/*.spec.ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  testEnvironment: 'node',
+};
+
+export default config;
